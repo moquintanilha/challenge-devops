3db561c6c040bb10eda348eed18133adc83c7f48
Marcos Quintanilha <marcos.quintanilha@mercadolivre.com>
2023-03-30 11:12:01 -0300

    add unit tests. :horse:

---
+++ tests/test_ping.py
@@ -0,0 +1,7 @@
+import requests
+
+# @pytest.mark.asyncio
+def test_ping():
+    response = requests.get('http://localhost:8080/ping')
+    status_code = response.status_code
+    assert status_code == 200
